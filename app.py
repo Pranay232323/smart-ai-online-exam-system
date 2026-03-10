5713691b995a7b47cc716b4d1995883e4a19fe32
Pranay Shinde <shindepranay922@gmail.com>
2026-03-10 21:00:50 +0530

    what you did

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
+
+# MySQL connection
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="@Pranay23",
+    database="exam_system"
+)
+
+cursor = db.cursor()
 
 @app.route("/")
 def home():
     return render_template("login.html")
+
+@app.route("/test-db")
+def test_db():
+    cursor.execute("SELECT * FROM users")
+    result = cursor.fetchall()
+    return str(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
