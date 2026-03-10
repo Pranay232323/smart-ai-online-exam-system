8bd40292c31efc1a8e7a98f6f008ee61028dd384
Pranay Shinde <shindepranay922@gmail.com>
2026-03-10 21:25:18 +0530

    Phase 3: Student registration system implemented

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import mysql.connector
 
 app = Flask(__name__)
@@ -22,6 +22,25 @@ def test_db():
     cursor.execute("SELECT * FROM users")
     result = cursor.fetchall()
     return str(result)
+# ------Register Page-------
+@app.route("/register")
+def register():
+    return render_template("register.html")
+#------Save user to database------
+
+@app.route("/register-user", methods=["POST"])
+def register_user():
+    name = request.form["name"]
+    email = request.form["email"]
+    password = request.form["password"]
+
+    sql = "INSERT INTO users (name, email, password, role) VALUES (%s, %s, %s, %s)"
+    values = (name, email, password, "student")
+
+    cursor.execute(sql, values)
+    db.commit()
+
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
